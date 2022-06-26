9c3c41187e52ad3dfd1e9197d2d093f020f5a2f7
rafaa <rfaesam0@gmail.com>
2022-06-26 05:14:02 +0000

    commit

--- scripts/lidar.py
+++ scripts/lidar.py
@@ -1,4 +1,6 @@
-from vis import Vis
+import sys, os
+sys.path.append(os.path.abspath(os.path.join('./')))
+from visualization import Vis
 import geopandas as gpd
 from fetch_data import FetchLidar
 from sub_sampler import SubSampler
@@ -6,14 +8,14 @@ from shapely.geometry import Polygon
 from fcache.cache import FileCache
 
 
-class PythonLidar:
-  """ PythonLidar is an open-source python package for retrieving, transforming, and visualizing point cloud data obtained through an aerial LiDAR survey. Using the package, you can select a region of interest, and download the related point cloud dataset with its metadata in different file formats (.laz, .tif, or as an ASCII file), perform transformation and visualization using the downloaded data.
+class LiDAR:
+  """ Lidar is an open-source python package for retrieving, transforming, and visualizing point cloud data obtained through an aerial LiDAR survey. Using the package, you can select a region of interest, and download the related point cloud dataset with its metadata in different file formats (.laz, .tif, or as an ASCII file), perform transformation and visualization using the downloaded data.
   """
   def __init__(self, epsg=26915):
     self.output_epsg = epsg
     self._input_epsg = 3857
     self._fetch_lidar = FetchLidar(self.output_epsg)
-    self._cache = FileCache('PythonLidar')
+    self._cache = FileCache('LiDAR')
 
   def fetch_lidar(self, polygon: Polygon, regions=[]):
     return self._fetch_lidar.fetch_lidar_data(polygon, regions)
